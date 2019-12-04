136efcf7ba1674e97d397d3b1bda6f6630d63d8f
joyce zhu <yirongzhu1@gmail.com>
2019-12-04 11:53:46 +0800

    addcreateactivitypage

---
+++ pages/createactivity/createactivity.js
@@ -0,0 +1,90 @@
+// pages/createactivity/createactivity.js
+const app = getApp()
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  // formSubmit: function (event) {
+  //   const form = {}
+  //   console.log(event);
+  //   form.name = event.detail.value.name
+  //   form.description = event.detail.value.description
+  //   form.date = event.detail.value.date
+  //   form.user_id = app.globalData.userId
+  //   console.log(10, form.user_id)
+  //   console.log(77, form);
+  //   wx.request({
+  //     url: `##`,
+  //     method: 'POST',
+  //     data: form,
+  //     success(res) {
+  //       console.log(res)
+  //       // redirect to index page when done
+  //       wx.reLaunch({
+  //         url: '/pages/myactivities/myactivities',
+  //       })
+  //     }
+  //   });
+  // },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
